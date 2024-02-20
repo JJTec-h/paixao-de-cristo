258b16ed76e8132c96dd5d37cb6858ac3e99651e
Edvaldojs22 <jonas.edvaldo.10@gmail.com>
2024-02-20 17:33:30 -0300

    att

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 
+function scrollToAnchor(event) {
+    event.preventDefault();
+
+    const targetId = event.currentTarget.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: "smooth"
+
+        });
+    }
+    console.log('ola')
+}
+
+const smoothScrollLinks = document.querySelectorAll(".smooth-scroll");
+smoothScrollLinks.forEach(function (link) {
+    link.addEventListener("click", scrollToAnchor);
+    console.log('ola')
+
+});
+
 
 const menu = document.querySelector('.menu');
 const painelMenu = document.querySelector('.painelMenu');
@@ -18,10 +41,21 @@ menu.addEventListener('click', () => {
 
 })
 
-
 links.forEach(link => {
     link.addEventListener('click', () => {
         painelMenu.classList.remove('menuAtivo')
         test = false;
     })
 })
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
